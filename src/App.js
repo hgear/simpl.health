ca1d2cf3519267b9915aa419d0e5249e5509a8c0
Faran Rashid <faran.rashid@hautelook.com>
2020-03-11 13:01:11 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+/*function test() {
+  //var a = 1;
+}*/
+const App = () => {
+  return <div>TEST</div>;
+};
+//var a = 1;
+
+//ReactDOM.render();
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
