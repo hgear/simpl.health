918f8ec34cb42d3f3449cd93a01011c384503e8c
Faran Rashid <faran.rashid@hautelook.com>
2020-05-27 07:22:11 -0700

    React Learning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 import SearchParams from "./SearchParams";
 import { Router, Link } from "@reach/router";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 /*function test() {
   //var a = 1;
 }*/
 const App = () => {
+  const themeHook = useState("peru");
   return (
     <React.StrictMode>
-      <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
 
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
     /*<div>
       <h1 id="something-important">Adopt Me!</h1>
